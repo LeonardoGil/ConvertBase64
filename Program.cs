50d3540d2e47507757268c0c7f90cb43c470db88
leonardo albuquerque gil <leonardoalbuquerquegil@hotmail.com>
2019-05-24 00:14:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diretorio = string.Empty;
+
+            Console.Write($"Informe um Diretorio: ");
+            diretorio = Console.ReadLine();
+
+            if (Repository.ExisteDiretorio(diretorio))
+            {
+                Console.WriteLine($"Diretorio {diretorio} encontrado.");
+                foreach (var arquivo in Repository.GetArquivosDiretorio(diretorio))
+                {
+                    Console.WriteLine($"Arquivo: {arquivo}");
+                    Repository.ConverterArquivoParBase64(arquivo);
+
+                    Console.WriteLine($"##############################################################################################");
+                }
+            }
+            else
+                Console.WriteLine($"Diretorio {diretorio} não existe.");
+
+            Console.WriteLine($"Finish");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConvertBase64
+{
+    public static class Repository
+    {
+        public static bool ExisteDiretorio(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return false;
+
+            return Directory.Exists(path); 
+        }
+
+        public static IEnumerable<string> GetArquivosDiretorio(string path)
+        {
+            IEnumerable<string> arquivos = Directory.EnumerateFiles(path);
+            Console.WriteLine(arquivos.Count() == 0 ? $"Nenhum arquivo encontrado" : $"{arquivos.Count()} arquivo(s) encontrado(s)");
+            return arquivos;
+        }
+
+        public static byte[] GetBytesArquivo(string nomearquivo)
+        {
+            byte[] conteudo = File.ReadAllBytes(nomearquivo);
+            return conteudo;
+        }
+
+        public static void ConverterArquivoParBase64(string nomearquivo)
+        {
+            var conteudo = GetBytesArquivo(nomearquivo);
+
+            if (conteudo == null)
+            {
+                Console.WriteLine($"Arquivo Vazio");
+                return;
+            }
+
+            try
+            {
+                string conteudoConvertido = Convert.ToBase64String(conteudo);
+                var arquivo = string.Concat(nomearquivo.Replace(".txt", string.Empty), "Base64.txt");
+
+                if (File.Exists(arquivo))
+                {
+                    Console.WriteLine($"Arquivo ja existe");
+                    return;
+                }    
+                else
+                    File.AppendAllText(arquivo, conteudoConvertido);
+
+                Console.WriteLine($"Arquivo {arquivo} Criado com sucesso.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
